c999a72f30143d6db809db427dd6860bf4b717db
atharva009 <atharvahankare@gmail.com>
2022-12-01 20:58:06 +0530

    add meals component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Modal from "../UI/Modal";
+
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "1", name: "Item", price: "10" }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.6</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
